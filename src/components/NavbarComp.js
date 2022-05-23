bf48e16b86060a4a143f2c0c3f3f58d4f956128d
Kai Jenkins <86726403+KMJENK17@users.noreply.github.com>
2022-05-23 15:20:13 -0400

    fixing navbar css and making it more responsive

--- src/components/NavbarComp.js
+++ src/components/NavbarComp.js
@@ -3,6 +3,7 @@ import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
 import Offcanvas from 'react-bootstrap/Offcanvas'
+import '../CSS/navbar.css'
 
 function NavbarComp() {
   return (
@@ -10,7 +11,7 @@ function NavbarComp() {
       {['xl'].map((expand) => (
     <Navbar key={expand} bg="dark" variant="dark" expand={expand} className="mb-3">
       <Container fluid>
-      <Navbar.Brand href="/home">Kai Jenkins</Navbar.Brand>
+      <Navbar.Brand href="/home" className="navbarBrandName">Kai Jenkins</Navbar.Brand>
         <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
         <Navbar.Offcanvas
           id={`offcanvasNavbar-expand-${expand}`}
@@ -18,8 +19,8 @@ function NavbarComp() {
           placement="end"
         >
           <Offcanvas.Header closeButton>
-            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-              Offcanvas
+            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`} className="offCanvasTitle">
+              Kai Jenkins
             </Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
@@ -29,7 +30,7 @@ function NavbarComp() {
             <Nav.Link href="/Experience">Experience</Nav.Link>
             <Nav.Link href="/Contact-Me">Contact Me</Nav.Link>
             <a href='../src/Kai-Jenkins-Resume.docx.pdf' download="Kai-Jenkins-Resume.pdf">
-            <Button variant="info">Download Resume</Button>{' '}
+            <Button variant="info" className="downloadResumeButton">Download Resume</Button>{' '}
             </a>
             </Nav>
             
